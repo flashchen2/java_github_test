f85f6fb530b251d0195daa245e1c3cfda430873f
flashchen2 <115390152+flashchen2@users.noreply.github.com>
2024-06-18 01:50:19 +0800

    修改主函数

--- github_test/src/main/java/com/zc/Main.java
+++ github_test/src/main/java/com/zc/Main.java
@@ -2,6 +2,8 @@ package com.zc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        这是一个注释
+        String s = "你好";
+        System.out.println("Hello world!"+s);
     }
 }
